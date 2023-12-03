0246176e7f9b53d7e0c12f8ca399087b08d62368
Nameless <truemedian@gmail.com>
2023-12-03 10:42:50 -0600

    day 3

--- src/day03.zig
+++ src/day03.zig
@@ -10,7 +10,103 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day03.txt");
 
-pub fn main() !void {}
+fn isSymbol(c: u8) bool {
+    return !std.ascii.isDigit(c) and c != '.';
+}
+
+const Number = struct {
+    value: usize,
+
+    row: usize,
+    col: usize,
+    length: u8,
+};
+
+const Symbol = struct {
+    row: usize,
+    col: usize,
+    char: u8,
+
+    pub fn isInRectangle(self: Symbol, x: usize, y: usize, width: usize) bool {
+        const left = if (x == 0) x else x - 1;
+        const right = x + width;
+
+        const top = if (y == 0) y else y - 1;
+        const bottom = y + 1;
+
+        return self.col >= left and self.col <= right and self.row >= top and self.row <= bottom;
+    }
+};
+
+pub fn main() !void {
+    @setEvalBranchQuota(100000);
+
+    var timer = std.time.Timer.start() catch unreachable;
+
+    var numbers = try std.ArrayList(Number).initCapacity(gpa, 1000);
+    var symbols = try std.ArrayList(Symbol).initCapacity(gpa, 1000);
+
+    var lines_it = tokenizeSca(u8, data, '\n');
+    var row: usize = 0;
+    while (lines_it.next()) |line| : (row += 1) {
+        var num: usize = 0;
+        var length: u8 = 0;
+
+        for (line, 0..) |c, col| {
+            if (std.ascii.isDigit(c)) {
+                num *= 10;
+                num += c - '0';
+                length += 1;
+            } else if (num != 0) {
+                try numbers.append(Number{ .value = num, .row = row, .col = col - length, .length = length });
+                num = 0;
+                length = 0;
+            }
+
+            if (isSymbol(c)) {
+                try symbols.append(Symbol{ .row = row, .col = col, .char = c });
+            }
+        }
+
+        if (num != 0) {
+            try numbers.append(Number{ .value = num, .row = row, .col = line.len - length, .length = length });
+        }
+    }
+
+    var sum1: usize = 0;
+    var sum2: usize = 0;
+
+    var arena = std.heap.ArenaAllocator.init(gpa);
+    defer arena.deinit();
+
+    for (symbols.items) |s| {
+        if (s.char == '*') {
+            var candidates = std.ArrayList(Number).init(arena.allocator());
+            defer _ = arena.reset(.retain_capacity);
+
+            for (numbers.items) |n| {
+                if (s.isInRectangle(n.col, n.row, n.length)) {
+                    try candidates.append(n);
+                    sum1 += n.value;
+                }
+            }
+
+            if (candidates.items.len == 2) {
+                sum2 += candidates.items[0].value * candidates.items[1].value;
+            }
+        } else {
+            for (numbers.items) |n| {
+                if (s.isInRectangle(n.col, n.row, n.length)) {
+                    sum1 += n.value;
+                }
+            }
+        }
+    }
+
+    print("Taken: {}\n", .{timer.read()});
+    print("Part 1: {}\n", .{sum1});
+    print("Part 2: {}\n", .{sum2});
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
